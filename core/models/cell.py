58b0d09283c4342e021b99608c804cc17ae36357
Dino <dino8890@protonmail.com>
2021-09-22 13:42:21 +0200

    Removed all references to cell variables.

--- core/models/cell.py
+++ core/models/cell.py
@@ -1,5 +1,5 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
-from django.db.models import CASCADE, FloatField, ForeignKey, IntegerField, ManyToManyField, Model
+from django.db.models import CASCADE, FloatField, ForeignKey, IntegerField, Model
 
 
 class Cell(Model):
@@ -9,19 +9,7 @@ class Cell(Model):
     classification = FloatField(validators=(MaxValueValidator(1.0), MinValueValidator(0.0)))
     order = IntegerField(validators=(MinValueValidator(0), MaxValueValidator(29)))
     document = ForeignKey('core.Document', CASCADE, 'cells')
-    variables = ManyToManyField('core.Variable',  through='core.CellVariable',)
 
     @property
     def user(self):
         return self.document.user  # required for object permission check
-
-
-class CellVariable(Model):
-    class Meta:
-        unique_together = ('cell', 'variable')
-
-    cell = ForeignKey('core.Cell', CASCADE)
-    variable = ForeignKey('core.Variable', CASCADE)
-    num_ontology_categories = IntegerField(validators=(MinValueValidator(1),))
-    num_keyword_vars = IntegerField(validators=(MinValueValidator(1),))
-    strength = FloatField(validators=(MaxValueValidator(1.0), MinValueValidator(0.0)))
